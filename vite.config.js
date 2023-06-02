890a6c18064fb1ed8c035b08df719d379fdfdb7f
Christian Gruenwald <christiangruenwald@gmx.net>
2023-06-02 18:08:25 +0200

    remove cache every reload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/christiang-dev.github.io/",
+  // base: "/",
   plugins: [svelte()],
 })
